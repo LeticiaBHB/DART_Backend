ecc477d79a92c87430f94d37f4be60924c24a836
bowie <leticia.bini@gmail.com>
2023-11-18 01:09:10 -0300

    commit

---
+++ BACKEND_BIN/bin/infra/custom_server.dart
@@ -0,0 +1,14 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+class CustomServer{
+
+  Future<void> initialize (Handler handler) async {
+
+    String address = 'localhost';
+    int port = 8080;
+
+    await shelf_io.serve( handler, address, port);
+    print('servidor inicializado -> http://$address:$port');
+  }
+}

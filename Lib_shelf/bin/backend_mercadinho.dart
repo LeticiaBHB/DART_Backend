e18b18184f72832cbfe7b9f70f29c88c29fe4133
bowie <leticia.bini@gmail.com>
2023-11-18 01:05:57 -0300

    commit

--- Lib_shelf/bin/backend_mercadinho.dart
+++ Lib_shelf/bin/backend_mercadinho.dart
@@ -5,32 +5,24 @@ import'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
 void main(List<String> arguments) async{
   var app = Router();
-  app.get('/',_handle);
+  app.get('/',(Request){
+    return Response(200, body: json, headers:{
+      'content-type':'text/json',
+    });
+  });
+
+  app.get('/html',(Request){
+    return Response(200, body: html, headers:{
+      'content-type':'text/html',
+    });
+  });
 
 
 
-  final server = await shelf_io.serve(_handle, '0.0.0.0', 4141);
+  final server = await shelf_io.serve(app, '0.0.0.0', 4141);
   print('server online: port ${server.port}');
 }
-
-FutureOr<Response> _handle(Request request){
-  print(request);
-    //return Response(200, body: 'funcionando!!');
-    //return Response(200, body: html);  -> aqui vai retornar um texto
-    //if(request.url.path =='json'){
-      return Response(200, body: json, headers:{
-      'content-type':'json',
-    });
-    }
     
-    //if(request.url.path =='index.html'){
-    //  return Response(200, body: html, headers:{
-    //  'content-type':'text/html',
-    //});
-    //}
-    //return Response(404);
-//}
-
 final json = '''
 {
   'message':'tudo certo aqui'

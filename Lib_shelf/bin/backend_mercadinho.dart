6992232fb3ce72b497f8a5d4a8a66b5a16eff446
bowie <leticia.bini@gmail.com>
2023-11-18 00:55:10 -0300

    commit - usando o _handle

--- Lib_shelf/bin/backend_mercadinho.dart
+++ Lib_shelf/bin/backend_mercadinho.dart
@@ -2,7 +2,13 @@ import 'dart:async';
 
 import 'package:shelf/shelf.dart';
 import'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart';
 void main(List<String> arguments) async{
+  var app = Router();
+  app.get('/',_handle);
+
+
+
   final server = await shelf_io.serve(_handle, '0.0.0.0', 4141);
   print('server online: port ${server.port}');
 }
@@ -11,19 +17,19 @@ FutureOr<Response> _handle(Request request){
   print(request);
     //return Response(200, body: 'funcionando!!');
     //return Response(200, body: html);  -> aqui vai retornar um texto
-    if(request.url.path =='json'){
+    //if(request.url.path =='json'){
       return Response(200, body: json, headers:{
       'content-type':'json',
     });
     }
     
-    if(request.url.path =='index.html'){
-      return Response(200, body: html, headers:{
-      'content-type':'text/html',
-    });
-    }
-    return Response(404);
-}
+    //if(request.url.path =='index.html'){
+    //  return Response(200, body: html, headers:{
+    //  'content-type':'text/html',
+    //});
+    //}
+    //return Response(404);
+//}
 
 final json = '''
 {

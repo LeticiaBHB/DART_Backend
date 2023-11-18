ea85264691ff8f705e97bcd9c41ac0c3ce05c022
bowie <leticia.bini@gmail.com>
2023-11-18 00:49:33 -0300

    commit

---
+++ Lib_shelf/bin/backend_mercadinho.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:shelf/shelf.dart';
+import'package:shelf/shelf_io.dart' as shelf_io;
+void main(List<String> arguments) async{
+  final server = await shelf_io.serve(_handle, '0.0.0.0', 4141);
+  print('server online: port ${server.port}');
+}
+
+FutureOr<Response> _handle(Request request){
+  print(request);
+    //return Response(200, body: 'funcionando!!');
+    //return Response(200, body: html);  -> aqui vai retornar um texto
+    if(request.url.path =='json'){
+      return Response(200, body: json, headers:{
+      'content-type':'json',
+    });
+    }
+    
+    if(request.url.path =='index.html'){
+      return Response(200, body: html, headers:{
+      'content-type':'text/html',
+    });
+    }
+    return Response(404);
+}
+
+final json = '''
+{
+  'message':'tudo certo aqui'
+}
+
+''';
+
+final html = '''
+<!DOCTYPE html>
+<html>
+<head>
+<title>DART</title>
+</head>
+<body>
+
+<h1>Este é um html em dart</h1>
+<p>This is a dart</p>
+
+</body>
+</html>
+
+
+''';
---
+++ Lib_shelf/test/backend_mercadinho_test.dart
@@ -0,0 +1,8 @@
+import 'package:backend_mercadinho/backend_mercadinho.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
